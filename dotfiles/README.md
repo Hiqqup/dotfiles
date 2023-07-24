da74cfa47263b004866ed93e96f053c294be7e8c
hiqqup <jubu6000@gmail.com>
2023-07-24 13:20:38 +0200

    add my config files

---
+++ dotfiles/README.md
@@ -0,0 +1,3 @@
+Installed debian with kde
+those are my config files
+gotta add bootstrap in order to install packages
---
+++ dotfiles/add.sh
@@ -0,0 +1,7 @@
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/dotfiles/add.sh
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/dotfiles/README.md
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/.config/awesome
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/.config/neofetch
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/.config/alacritty.yml
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/.config/picom.conf
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME add $HOME/.config/nvim
