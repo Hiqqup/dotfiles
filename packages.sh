b4baf7cecf2ef32a59ba1130e57d30089af4babd
hiqqup <jubu6000@gmail.com>
2023-10-12 22:35:28 +0200

    xbacklight packages added

--- packages.sh
+++ packages.sh
@@ -1,2 +1,2 @@
-sudo pacman -S xorg-xinit xorg-server awesome alacritty neovim feh rofi xclip xdotool fzf tmux keepassxc ttf-jetbrains-mono-nerd xorg-xrandr xf86-input-wacom ripgrep syncthing alsa-utils openssh htop unzip
+sudo pacman -S xorg-xinit xorg-server awesome alacritty neovim feh rofi xclip xdotool fzf tmux keepassxc ttf-jetbrains-mono-nerd xorg-xrandr xf86-input-wacom ripgrep syncthing alsa-utils openssh htop unzip xorg-xbacklight acpi
 
