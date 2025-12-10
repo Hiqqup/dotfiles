9adf2069d6d877f8b6e45635f33d45db28d9ef77
hiqqup <71655937+Hiqqup@users.noreply.github.com>
2025-12-10 19:24:10 +0100

    clean all

--- home/.config/my-scripts/latex/mdToPdf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-lowdown -s -tlatex --parse-math $1 > /tmp/e.tex && pdflatex /tmp/e.tex
-
-# lowdown -s -tlatex --parse-math Restklassen.md > /tmp/e.tex && pdflatex -output-directory=/tmp /tmp/e.tex && zathura /tmp/e.pdf
--- home/.config/nvim/init.lua
+++
@@ -1,4 +0,0 @@
-vim.g.vaultdir = vim.fn.expand "~" .. "/notes";
-require("vanilla");
-require("lazy-setup");
-require("obsidian.setup")();
