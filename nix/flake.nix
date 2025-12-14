25bc7523d309032255a6d8ba80820940863cfc33
juneal <71655937+Hiqqup@users.noreply.github.com>
2025-12-14 16:00:03 +0100

    add flake

---
+++ nix/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      username = "ju";
+    in {
+      homeConfigurations.${username} =
+        home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          modules = [
+            {
+              home.username = username;
+              home.homeDirectory = "/home/${username}";
+              home.stateVersion = "24.05";
+
+              programs.home-manager.enable = true;
+
+              home.packages = [
+                pkgs.hello
+              ];
+            }
+          ];
+        };
+    };
+}
